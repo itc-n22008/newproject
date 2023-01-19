dba97e4547d4ab39c4ae46fead2665b953a8f251
itc-n22008 <n22008@std.it-college.ac.jp>
2023-01-19 07:43:18 +0000

    更新

--- components/page1.module.js
+++ components/page1.module.js
@@ -4,14 +4,14 @@ export default function PAGE1() {
 	return (
 	<div>
 	<div className={styles.header}>
-    	<h1>Sample Website</h1>
+    	<h1>W我們去打疫苗吧
+Wǒmen qù dǎ yìmiáo baarning!You got a virus</h1>
 	</div>
 	
     	<div className={styles.main}>
 	<h2>Heading</h2>
     		<p>
-        	Content comes here. Content comes here. Content comes here.
-        	 Content comes here. Content comes here. Content comes here.
+		भवतः वायरसः अभवत्अतीव भयङ्करं भवति
     	</p>
     	<h2>Heading 2</h2>
     		<p>
